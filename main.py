5846badeaea7c972bd4baaee0dcbd01801172d9f
Joel Benjamin <joelbenjamin093@gmail.com>
2017-07-03 20:57:42 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,18 +1,19 @@
 import time
 import random
-from flask import Flask, request, Response
+from flask import Flask, request, Response,render_template
 from kik import KikApi, Configuration
 from kik.messages import messages_from_json,TextMessage
 #from tag_speech import SpeechTagger
 # import responses
 from responses import UsrgreetingList,messageList,help_str
-
+from weather_request import *
 from wit import Wit
 
-bot = Wit(access_token="access-code")
+bot = Wit(access_token="VOCFYQI6MHUL7CDRMPURE4Z26KJGNL7G")
 
-usr_name = 'kik-bot-name'
-key = 'key'
+usr_name = 'glooffybot'
+key = '71981fb3-c57d-43bf-ac84-534120e8857e'
+weather_key = "396cbf110cc7bd1bd3087f317643a83f"
 
 app = Flask(__name__)
 kik = KikApi(usr_name, key)
@@ -23,7 +24,7 @@ kik = KikApi(usr_name, key)
 kik.set_configuration(Configuration(webhook='https://glooffy.herokuapp.com/'))
 
 def SendMessage(user,ch_id,msg):
-	# sends a single message 
+	# sends a single message
 	kik.send_messages([TextMessage(to=user,chat_id=ch_id,body=msg)])
 
 @app.route('/', methods=['POST'])
@@ -31,8 +32,8 @@ def incoming():
 	if not kik.verify_signature(request.headers.get('X-Kik-Signature'), request.get_data()):
 		return Response(status=403)
 
-    # list of python objects 
-	messages = messages_from_json(request.json['messages'])	
+    # list of python objects
+	messages = messages_from_json(request.json['messages'])
 	user_name = messages[0].from_user
 	chatting_id = messages[0].chat_id
 	current_msg = messages[0].body.lower()
@@ -45,28 +46,45 @@ def incoming():
 	entity = "#other"
 	try:
 		entity = list(resp['entities'])[0]
+		try:
+			# store the location if available
+			location_entity = list(resp['entities'])[1]
+			# get the location name
+			location = resp['entities']['location'][0]['value']
+		except:
+			pass
 	except:
 		pass
 	if entity == "greetings":
-		greeting_list = ["hey-o","Yo...woff woff","Hi-yo","mE COol dOG, yOU?"]
+		greeting_list = ["hey-o","Yo","Hi-yo","me cool, you?","hi","hey"]
 		SendMessage(user_name,chatting_id,greeting_list[random.randint(0,len(greeting_list)-1)])
 	elif entity == "fine_tag":
 		fine_resp = ["That's great!","Nice!","Cool, Human!","good to know"]
 		SendMessage(user_name,chatting_id,fine_resp[random.randint(0,len(fine_resp)-1)])
 	elif entity == "ask_fine":
-		ask_fine_resp = ["Yeah mE cOOl","I'm cool!","I'm well","yeah, I'm awesome!","I'm fine"]
+		ask_fine_resp = ["Yeah me cool!","I'm cool!","I'm well","yeah, I'm awesome!","I'm fine"]
 		SendMessage(user_name,chatting_id,ask_fine_resp[random.randint(0,len(ask_fine_resp)-1)])
 		# get the weather
 	elif entity == "get_weather":
+		# if location is available get the weather with location, else
+		# get the weather of the current place
 		SendMessage(user_name,chatting_id,"Wait a sec...")
-		SendMessage(user_name,chatting_id,"Let me get it for you.")
+		try:
+			SendMessage(user_name,chatting_id,"here's the weather")
+			wex = WexExtractor(location,weather_key)
+			data = wex.GetJsonData()
+			weather_msg = data['weather'][0]['description']
+			SendMessage(user_name,chatting_id,weather_msg)
+		except:
+			SendMessage(user_name,chatting_id,"current location weather")
+
 	else:
-		SendMessage(user_name,chatting_id,"Sorry I didn't catch that!")
+		SendMessage(user_name,chatting_id,"Sorry, didn't catch that!")
 	return Response(status=200)
 
 @app.route('/')
 def show_index_page():
-	return "<h1>Welcome to glooffy bot!</h1>"
+	return render_template("index.html")
 
 
 if __name__ == "__main__":
